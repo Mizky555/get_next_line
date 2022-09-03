4d395b7cba4b0e7656d6d035eff249e37251658f
tsirirak <tsirirak@42.fr>
2022-09-04 01:08:00 +0700

    while joinstr buf1 buf2 ...

--- get_next_line.c
+++ get_next_line.c
@@ -2,30 +2,31 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include "get_next_line.h"
 
 char get_next_line(int fd)
 {
     char *buf;
-    size_t len;
+    size_t len_buf;
 
     buf = (char *)malloc(BUFFER_SIZE * (sizeof(char)) + 1);
     while (1)
     {
-        len = read(fd, buf, BUFFER_SIZE);
+        len_buf = read(fd, buf, BUFFER_SIZE);
         int i;
         char *srtjoy;
         i = 0;
-        while (i < len)
+        while (i < len_buf)
         {
             if (buf[i] == "\n")
             {
                 return (srtjoy);
             }
-            
+            i++;
         }
     }
-    buf[len] = '\0';
-    printf("len = %zu,*buf = %s",len, buf);
+    buf[len_buf] = '\0';
+    printf("len_buf = %zu,*buf = %s",len_buf, buf);
     return(0);
 
 }
---
+++ get_next_line.h
@@ -0,0 +1,10 @@
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
+int	ft_strlcpy(char *dst, const char *src, size_t size);
+
+#endif
