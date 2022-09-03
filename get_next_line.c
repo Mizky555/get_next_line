f6c4c34acd57e17f73b1f5eddb0d4c051298455c
Mizky555 <102453410+Mizky555@users.noreply.github.com>
2022-09-03 19:15:10 +0700

    09.15

--- get_next_line.c
+++ get_next_line.c
@@ -5,11 +5,11 @@
 
 char get_next_line(int fd)
 {
-    void *buf;
+    char *buf;
     size_t len;
 
     len = read(fd ,*buf ,5);
-    printf("len = %zu,*buf = %p",len, buf);
+    printf("len = %zu,*buf = %s",len, buf);
     return(0);
 
 }
