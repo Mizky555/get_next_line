5a737d0b9c926a3b7c41e4140aca6f8efef400b9
Mizky555 <102453410+Mizky555@users.noreply.github.com>
2022-09-03 19:37:47 +0700

    19.37

--- get_next_line.c
+++ get_next_line.c
@@ -8,8 +8,9 @@ char get_next_line(int fd)
     char *buf;
     size_t len;
 
-    len = read(fd ,*buf ,5);
-    buf = (char *)malloc((len * (sizeof(char)) + 1);
+    
+    buf = (char *)malloc((5 * (sizeof(char)) + 1);
+    len = read(fd ,*buf , 5);
     printf("len = %zu,*buf = %s",len, *buf);
     return(0);
 
