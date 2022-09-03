2897b4efe8bfe6647327249e5f8d9ac2791de355
Gamer <gamer42bkk@z3t11c1.42bangkok.com>
2022-09-03 23:44:34 +0700

    test

--- get_next_line.c
+++ get_next_line.c
@@ -8,11 +8,23 @@ char get_next_line(int fd)
     char *buf;
     size_t len;
 
-    
     buf = (char *)malloc(BUFFER_SIZE * (sizeof(char)) + 1);
-
-    len = read(fd ,buf , BUFFER_SIZE);
-  //  buf[len] = '\0';
+    while (1)
+    {
+        len = read(fd, buf, BUFFER_SIZE);
+        int i;
+        char *srtjoy;
+        i = 0;
+        while (i < len)
+        {
+            if (buf[i] == "\n")
+            {
+                return (srtjoy);
+            }
+            
+        }
+    }
+    buf[len] = '\0';
     printf("len = %zu,*buf = %s",len, buf);
     return(0);
 
