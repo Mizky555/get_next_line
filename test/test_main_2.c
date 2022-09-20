d6d1a6cc6a8addf5186d73e82de5170176f37cfa
Mizky555 <102453410+Mizky555@users.noreply.github.com>
2022-09-20 22:59:47 +0700

    22:59

--- test/test_main_2.c
+++ test/test_main_2.c
@@ -1,21 +1,65 @@
 #include "main.h"
 
+char *ft_strjoin(char *s1, char *s2)
+{
+	char *str;
+	size_t i;
+	size_t len;
+	int j1;
+	int j2;
+
+	i = 0;
+	j1 = 0;
+	j2 = 0;
+	len = ft_strlen(s1) + ft_strlen(s2);
+	str = malloc(sizeof(char) * len + 1);
+
+	while (i < len)
+	{
+		if (i < ft_strlen(s1))
+			str[i] = s1[j1++];
+		else if (i < len)
+			str[i] = s2[j2++];
+		i++;
+	}
+	str[i] = '\0';
+	if (s1)
+	{
+		free(s1);
+		s1 = NULL;
+	}
+	return (str);
+}
+
 char    *get_next_line(int fd)
 {
     char    *buf;
     int len_buf;
     static  box b;
+    char    *str;
 
     buf = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
-    len_buf = read(fd,buf,BUFFER_SIZE);
-    b.str = buf;
-    return(b.str);
-
+    b.str = (char *)malloc(sizof(char) * BUFFER_SIZE + 1);
     while (1)
     {
-        while (BUFFER_SIZE>)
+        int i_buf;
+
+        i_buf = 0;
+        buf = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+        len_buf = read(fd, buf ,BUFFER_SIZE);
+        while (BUFFER_SIZE > i_buf)
+        {
+            if (buf[i] == '\n' || buf[i] == '\0')
+            {
+                b.str = buf;
+                return (str);
+            }
+            str = ft_strjoin(str, buf[i]);
+            i++;
+        }
     }
 }
+
 
 int main()
 {
