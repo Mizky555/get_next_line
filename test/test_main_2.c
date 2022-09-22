719b23d9861e1024d3bf7dccd03b4e1cc64aa4d9
Mizky555 <102453410+Mizky555@users.noreply.github.com>
2022-09-22 23:44:07 +0700

    a

--- test/test_main_2.c
+++ test/test_main_2.c
@@ -1,40 +1,34 @@
 #include "main.h"
 
-size_t	ft_strlen(const char *s)
+size_t    ft_strlen(const char *s)
 {
-	int	i;
+    int    i;
 
-	i = 0;
-	while (s[i] != '\0')
-		i++;
-	return (i);
+    i = 0;
+    while (s[i] != '\0')
+        i++;
+    return (i);
 }
 
 char *ft_strjoin_cha(char *s1, char c)
 {
-	char *str;
-	size_t i;
-	size_t len;
-	int j1;
-	int j2;
+    char *str;
+    size_t i;
+    size_t len;
+    int j1;
+    int j2;
 
-	i = 0;
-	len = ft_strlen(s1) + 1;
-	str = malloc(sizeof(char) * len + 1);
-
-	while (s1[i] != '\0')
-	{
-		str[i] = s1[i];
-		i++;
-	}
-	str[i] = c;
-	str[i + 1] = '\0';
-	if (s1)
-	{
-		free(s1);
-		s1 = NULL;
-	}
-	return (str);
+    i = 0;
+    len = ft_strlen(s1) + 1;
+    str = malloc(sizeof(char) * len + 1);
+    while (s1[i] != '\0')
+    {
+        str[i] = s1[i];
+        i++;
+    }
+    str[i] = c;
+    str[i + 1] = '\0';
+    return (str);
 }
 
 char    *get_next_line(int fd)
@@ -43,40 +37,36 @@ char    *get_next_line(int fd)
     int len_buf;
     static  box b;
     char    *str;
-	int i_buf;
+    int i_buf;
 
-    b.str = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+    b.str = (char *) malloc(sizeof(char) * BUFFER_SIZE + 1);
+    i_buf = 0;
     while (1)
     {
-        
-
-        i_buf = 0;
         buf = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
         len_buf = read(fd, buf ,BUFFER_SIZE);
-		printf("len_buf  read   = %d\n", len_buf);
-		printf("BUFFER_SIZE = %d\n",BUFFER_SIZE);
-		printf("i_buf = %d\n",i_buf);
-		int buf_int = BUFFER_SIZE;
-        while (1)
+        i_buf = 0;
+        while(i_buf < BUFFER_SIZE)
         {
-			printf("while i_buf = %d",i_buf);
+            
+            // if (buf[i_buf] == '\n' || buf[i_buf] == '\0')
+            // {
+            //     return (str);
+            // }
+            // str = ft_strjoin_cha(str, buf[i_buf]);
+            // i_buf++;
             if (buf[i_buf] == '\n' || buf[i_buf] == '\0')
             {
-				return(0);
                 b.str = buf;
-				str = ft_strjoin_cha(str, buf[i_buf]);
-				printf("b join = %s\n",str);
-				if (buf[i_buf] != '\0')
-					str = ft_strjoin_cha(str, '\0');
+                str = ft_strjoin_cha(str, buf[i_buf]);
+                if (buf[i_buf] != '\0')
+                    str = ft_strjoin_cha(str, '\0');
                 return (str);
             }
             str = ft_strjoin_cha(str, buf[i_buf]);
-			printf("back join = %s\n",str);
             i_buf++;
-			return(0);
         }
-		return(0);
-		free(buf);
+        free(buf);
     }
 }
 
@@ -84,7 +74,9 @@ char    *get_next_line(int fd)
 int main()
 {
     int fd;
-    fd = open("opal_i",O_RDONLY);
-    printf("--get_next_line--\n%s",get_next_line(fd));
+    fd = open("opal_i", O_RDONLY);
+    printf("line = %s\n", get_next_line(fd));
+    printf("line = %s\n", get_next_line(fd));
+    printf("line = %s\n", get_next_line(fd));
     close(fd);
 }
