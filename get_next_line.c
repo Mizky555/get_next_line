cf25128ef6aa17848e7b745a3d8bb474e3607f00
Mizky555 <102453410+Mizky555@users.noreply.github.com>
2022-09-24 20:17:59 +0700

    42

--- get_next_line.c
+++ get_next_line.c
@@ -14,7 +14,7 @@ size_t    ft_strlen(const char *s)
     return (i);
 }
 
-char	*ft_strchr_l(const char *s, int c)
+char	*ft_strchr_l(const char *s)
 {
 	size_t	i;
 
@@ -32,59 +32,62 @@ char	*ft_strchr_l(const char *s, int c)
 
 
 
-// int	len_newline(char *buf,int len_buf){
-//     int i;
+int	len_newline(char *buf,int len_buf){
+    int i;
 
-//     i = 0;
-//     while(i <= len_buf)
-//     {
-//         if (buf[i] == '\n')
-//             return (i);
-//         i++;
-//     }
-//     return (len_buf);
-// }
-// char	*ft_strjoin(char const *s1, char const *s2, size_t len_s2)
-// {
-// 	char	*str;
-// 	size_t	i;
-// 	int		j1;
-// 	int		j2;
-// 	size_t	len;
+    i = 0;
+    while(i <= len_buf)
+    {
+        if (buf[i] == '\n')
+            return (i);
+        i++;
+    }
+    return (len_buf);
+}
+char	*ft_strjoin(char const *s1, char const *s2, size_t len_s2)
+{
+	char	*str;
+	size_t	i;
+	int		j1;
+	int		j2;
+	size_t	len;
 
-// 	len = ft_strlen(s1) + len_s2;
-// 	str = malloc(sizeof(char) * len + 1);
-// 	i = 0;
-// 	j1 = 0;
-// 	j2 = 0;
-// 	if (str == NULL)
-// 		return (NULL);
-// 	if (s1 == NULL && s2 == NULL)
-// 		return (NULL);
-// 	while (i < len)
-// 	{
-// 		if (i < ft_strlen(s1))
-// 			str[i] = s1[j1++];
-// 		else if (i < len)
-// 			str[i] = s2[j2++];
-// 		i++;
-// 	}
-// 	str[i] = '\0';
-// 	return (str);
-// }
+	len = ft_strlen(s1) + len_s2;
+	str = malloc(sizeof(char) * len + 1);
+	i = 0;
+	j1 = 0;
+	j2 = 0;
+	if (str == NULL)
+		return (NULL);
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		if (i < ft_strlen(s1))
+			str[i] = s1[j1++];
+		else if (i < len)
+			str[i] = s2[j2++];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
 
-// char get_next_line(int fd)
-// {
-//     char *buf;
-//     int len_buf;
-// 	char *str;
+char get_next_line(int fd)
+{
+    char *buf;
+    int len_buf;
+	char *str;
 
-//     buf = (char *) malloc(sizeof(char) * BUFFER_SIZE + 1);
-//     len_buf = read(fd, buf, BUFFER_SIZE);
-//     str = ft_strjoin(str,buf,len_newline(buf, len_buf));
-// 	b.str = ft_strchr_l(buf,'/n')
+    buf = (char *) malloc(sizeof(char) * BUFFER_SIZE + 1);
+    len_buf = read(fd, buf, BUFFER_SIZE);
+	printf("len_buf = %d",len_buf);
+    str = ft_strjoin(str,buf,len_newline(buf, len_buf));
+	printf("str strjoin(str,buf,num) = %s\n",str);
+	b.str = ft_strchr_l(buf)
+	printf("b.str ft_strchr = %s \n",b.str);
    
-// }
+}
 
 int main()
 {
