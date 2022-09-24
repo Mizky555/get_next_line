b55c68725a1381499d98f33dc5665900cac9ebd4
Mizky555 <102453410+Mizky555@users.noreply.github.com>
2022-09-24 16:08:10 +0700

    42

--- test/test_main_2.c
+++ test/test_main_2.c
@@ -45,8 +45,8 @@ char    *get_next_line(int fd)
     {
         buf = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
         len_buf = read(fd, buf ,BUFFER_SIZE);
-        if (b.buf == 0)
-            b.buf = 0;
+        if (b.len == 0)
+            b.len = 0;
         while(i_buf < BUFFER_SIZE)
         {
             
