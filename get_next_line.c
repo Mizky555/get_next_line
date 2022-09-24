dbf23321d70a6c82e38aed58705642da1067e235
Mizky555 <102453410+Mizky555@users.noreply.github.com>
2022-09-24 19:30:38 +0700

    24

--- get_next_line.c
+++ get_next_line.c
@@ -9,51 +9,15 @@ char	*ft_strchr_l(const char *s, int c)
 	int	i;
 
 	i = 0;
-	while (s[i] != '\n')
+	while (i <= ft_strlen(s))
 	{
-		if (s[i] == (char)c)
+		if (s[i] == '\n')
 		{
 			return ((char *)&s[i]);
 		}
 		i++;
 	}
-	if (c == '\0')
-		return ((char *)&s[i]);
-	return (0);
-}
-
-// char	*ft_strchr_r(const char *s, int c)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while (s[i] != '\n')
-// 	{
-// 		if (s[i] == (char)c)
-// 		{
-// 			return ((char *)&s[i]);
-// 		}
-// 		i++;
-// 	}
-// 	if (c == '\0')
-// 		return ((char *)&s[i]);
-// 	return (0);
-// }
-
-char	*ft_strchr_r(const char *s, int c)
-{
-	int	i;
-
-	i = 0;
-	while (s[i] != '\0')
-		i++;
-	while (i >= 0)
-	{
-		if (s[i] == (char)c)
-			return ((char *)(s + i));
-		i--;
-	}
-	return (0);
+	return (i);
 }
 
 size_t    ft_strlen(const char *s)
@@ -66,55 +30,57 @@ size_t    ft_strlen(const char *s)
     return (i);
 }
 
-int	len_newline(char *buf,int len_buf){
-    int i;
+// int	len_newline(char *buf,int len_buf){
+//     int i;
 
-    i = 0;
-    while(i <= len_buf)
-    {
-        if (buf[i] == '\n')
-            return (i);
-        i++;
-    }
-    return (len_buf);
-}
-char	*ft_strjoin(char const *s1, char const *s2, size_t len_s2)
-{
-	char	*str;
-	size_t	i;
-	int		j1;
-	int		j2;
-	size_t	len;
+//     i = 0;
+//     while(i <= len_buf)
+//     {
+//         if (buf[i] == '\n')
+//             return (i);
+//         i++;
+//     }
+//     return (len_buf);
+// }
+// char	*ft_strjoin(char const *s1, char const *s2, size_t len_s2)
+// {
+// 	char	*str;
+// 	size_t	i;
+// 	int		j1;
+// 	int		j2;
+// 	size_t	len;
 
-	len = ft_strlen(s1) + len_s2;
-	str = malloc(sizeof(char) * len + 1);
-	i = 0;
-	j1 = 0;
-	j2 = 0;
-	if (str == NULL)
-		return (NULL);
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
-	while (i < len)
-	{
-		if (i < ft_strlen(s1))
-			str[i] = s1[j1++];
-		else if (i < len)
-			str[i] = s2[j2++];
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
-}
+// 	len = ft_strlen(s1) + len_s2;
+// 	str = malloc(sizeof(char) * len + 1);
+// 	i = 0;
+// 	j1 = 0;
+// 	j2 = 0;
+// 	if (str == NULL)
+// 		return (NULL);
+// 	if (s1 == NULL && s2 == NULL)
+// 		return (NULL);
+// 	while (i < len)
+// 	{
+// 		if (i < ft_strlen(s1))
+// 			str[i] = s1[j1++];
+// 		else if (i < len)
+// 			str[i] = s2[j2++];
+// 		i++;
+// 	}
+// 	str[i] = '\0';
+// 	return (str);
+// }
 
 // char get_next_line(int fd)
 // {
 //     char *buf;
 //     int len_buf;
+// 	char *str;
 
 //     buf = (char *) malloc(sizeof(char) * BUFFER_SIZE + 1);
 //     len_buf = read(fd, buf, BUFFER_SIZE);
-//     len_newline(buf, len_buf);
+//     str = ft_strjoin(str,buf,len_newline(buf, len_buf));
+// 	b.str = ft_strchr_l(buf,'/n')
    
 // }
 
@@ -122,7 +88,6 @@ int main()
 {
     int	fd;
 		printf("ft_strchr_l = %s\n",ft_strchr_l("opal\n123",'a'));
-		printf("ft_strchr_r = %s\n",ft_strchr_r("opal\n1a23",'a'));
     //fd = open("opal_i", O_RDONLY);
     //printf("get_next_line = \n",get_next_line(fd));
 }
