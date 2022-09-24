c5d80be0ace89ae0c37dbf979fbcdb26146efa3d
Mizky555 <102453410+Mizky555@users.noreply.github.com>
2022-09-24 22:15:36 +0700

    b.str

--- get_next_line.c
+++ get_next_line.c
@@ -96,7 +96,7 @@ char *get_next_line(int fd)
 	printf("fd = %d\n",fd);
 		len_buf = read(fd, buf, BUFFER_SIZE);
 		printf("len_buf = %d\n",len_buf);
-    	str = ft_strjoin(str,buf,len_newline(buf, len_buf));
+    	str = ft_strjoin(b.str,buf,len_newline(buf, len_buf));
 		printf("str strjoin(str,buf,num) = %s\n",str);
 		b.str = ft_strchr_l(buf);
 		printf("b.str ft_strchr = %s \n",b.str);
