345338a143f656b2085494ac595a019988eaa049
Mizky555 <102453410+Mizky555@users.noreply.github.com>
2022-09-25 16:37:17 +0700

    main.c

--- get_next_line.c
+++ get_next_line.c
@@ -171,39 +171,3 @@ char *get_next_line(int fd)
 	return (str);
 }
 
-// int main(int argc, char **argv)
-// {
-//     int	fd;
-// 	char *str;
-
-// 	(void) argc;
-// 	str = NULL;
-// 	fd = open(argv[1], O_RDONLY);
-
-// 	str = get_next_line(fd);
-// 	printf("get_next_line = %s\n",str);
-// 	free(str);
-// 	str = NULL;
-
-// 	str = get_next_line(fd);
-// 	printf("get_next_line = %s\n",str);
-// 	free(str);
-// 	str = NULL;
-
-// 	str = get_next_line(fd);
-// 	printf("get_next_line = %s\n",str);
-// 	free(str);
-// 	str = NULL;
-
-// 	str = get_next_line(fd);
-// 	printf("get_next_line = %s\n",str);
-// 	free(str);
-// 	str = NULL;
-
-// 	str = get_next_line(fd);
-// 	printf("get_next_line = %s\n",str);
-// 	free(str);
-// 	str = NULL;
-
-// 	close(fd);
-// }
