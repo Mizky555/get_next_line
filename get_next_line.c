26973dc329ac5207980b4e8508a010515353532f
Mizky555 <102453410+Mizky555@users.noreply.github.com>
2022-09-25 16:34:46 +0700

    test again

--- get_next_line.c
+++ get_next_line.c
@@ -171,39 +171,39 @@ char *get_next_line(int fd)
 	return (str);
 }
 
-int main(int argc, char **argv)
-{
-    int	fd;
-	char *str;
+// int main(int argc, char **argv)
+// {
+//     int	fd;
+// 	char *str;
 
-	(void) argc;
-	str = NULL;
-	fd = open(argv[1], O_RDONLY);
+// 	(void) argc;
+// 	str = NULL;
+// 	fd = open(argv[1], O_RDONLY);
 
-	str = get_next_line(fd);
-	printf("get_next_line = %s\n",str);
-	free(str);
-	str = NULL;
+// 	str = get_next_line(fd);
+// 	printf("get_next_line = %s\n",str);
+// 	free(str);
+// 	str = NULL;
 
-	str = get_next_line(fd);
-	printf("get_next_line = %s\n",str);
-	free(str);
-	str = NULL;
+// 	str = get_next_line(fd);
+// 	printf("get_next_line = %s\n",str);
+// 	free(str);
+// 	str = NULL;
 
-	str = get_next_line(fd);
-	printf("get_next_line = %s\n",str);
-	free(str);
-	str = NULL;
+// 	str = get_next_line(fd);
+// 	printf("get_next_line = %s\n",str);
+// 	free(str);
+// 	str = NULL;
 
-	str = get_next_line(fd);
-	printf("get_next_line = %s\n",str);
-	free(str);
-	str = NULL;
+// 	str = get_next_line(fd);
+// 	printf("get_next_line = %s\n",str);
+// 	free(str);
+// 	str = NULL;
 
-	str = get_next_line(fd);
-	printf("get_next_line = %s\n",str);
-	free(str);
-	str = NULL;
+// 	str = get_next_line(fd);
+// 	printf("get_next_line = %s\n",str);
+// 	free(str);
+// 	str = NULL;
 
-	close(fd);
-}
+// 	close(fd);
+// }
