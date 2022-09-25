f0720d2fab0f76a5d1767958309b4abdfbccabaf
Mizky555 <102453410+Mizky555@users.noreply.github.com>
2022-09-25 16:46:22 +0700

    len

--- get_next_line.c
+++ get_next_line.c
@@ -141,6 +141,7 @@ char *get_next_line(int fd)
 		buf[len_buf] = 0;
 		str = ft_strjoin(str,buf,len_newline(buf, len_buf)); // ถ้าอ่านเจอจะจับ buf ที่จนถึง \n ไปต่อท้าย
 		printf("str = %s\n", str);
+		printf("len new line = %d\tlenbuf = %d\n", len_newline(buf, len_buf), len_buf);
 		if (len_newline(buf, len_buf) != len_buf) //ถ้าเจอ \n ใน buf จะถูกเตะออก
 			break;
 	}
