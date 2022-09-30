6c3004fc0214e1330326c2546f8241456442563e
Mizky555 <102453410+Mizky555@users.noreply.github.com>
2022-10-01 01:57:15 +0700

    c

--- get_test/get_next_line.c
+++ get_test/get_next_line.c
@@ -4,32 +4,6 @@
 #include <stdlib.h>
 #include "get_next_line.h"
 
-void	*ft_memset(void *b, int c, size_t len)
-{
-	size_t	i;
-
-	i = 0;
-	while (i < len)
-	{
-		((char *)b)[i] = c;
-		i++;
-	}
-	return (b);
-}
-
-int   ft_strlen(char *s)
-{
-    int    i;
-
-    i = 0;
-	if (s)
-	{
-		while (s[i] != '\0')
-			i++;
-	}
-	return (i);
-}
-
 char	*ft_strchr_l(char *s)
 {
 	int	i;
