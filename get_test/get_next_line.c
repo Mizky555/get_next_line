07a5b881f8cc0e4a0af48b97b0cfce1694d6eff2
Mizky555 <102453410+Mizky555@users.noreply.github.com>
2022-10-01 02:08:49 +0700

    i++

--- get_test/get_next_line.c
+++ get_test/get_next_line.c
@@ -74,10 +74,9 @@ char	*ft_strjoin(char *s1, char *s2, int len_s2)
 	if (str == NULL)
 		return (NULL);
 	ft_memset(str, 0, len + 1);
-	while (s1 != NULL && s1[i] != '\0')
+	while (s1 != NULL && s1[i++] != '\0')
 	{
 		str[i] = s1[i];
-		i++;
 	}
 	while (s2 != NULL && j < len_s2)
 		str[i++] = s2[j++];
