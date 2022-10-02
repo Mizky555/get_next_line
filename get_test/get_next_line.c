7ca4680964382e0a42ccab61590e2a7b021d4068
Mizky555 <102453410+Mizky555@users.noreply.github.com>
2022-10-03 02:29:52 +0700

    f

--- get_test/get_next_line.c
+++ get_test/get_next_line.c
@@ -16,56 +16,6 @@
 #include <stdlib.h>
 #include "get_next_line.h"
 
-char	*ft_strchr_l(char *s, int i)
-{
-	char	*str;
-
-	str = NULL;
-	if (s && s[i])
-	{
-		while (i < ft_strlen(s))
-		{
-			if (s[i] == '\n' && (i + 1) < ft_strlen(s))
-			{
-				str = ft_strjoin(str, &s[i + 1], ft_strlen(&s[i + 1]));
-				if (!str[0])
-				{
-					free(str);
-					str = NULL;
-				}
-				if (s)
-					free(s);
-				return (str);
-			}
-			i++;
-		}
-	}
-	if (s)
-		free(s);
-	return (NULL);
-}
-
-int	len_newline(char *buf, int len_buf, int mode)
-{
-	int	i;
-
-	i = 0;
-	if (buf)
-	{
-		while (i < len_buf)
-		{
-			if (buf[i] == '\n')
-				return (i + 1);
-			i++;
-		}
-		if (mode == 1)
-			return (len_buf);
-		else if (mode == 2)
-			return (0);
-	}
-	return (0);
-}
-
 char	*ft_strjoin(char *s1, char *s2, int len_s2)
 {
 	char	*str;
