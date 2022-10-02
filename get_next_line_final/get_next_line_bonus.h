eed47c905308812e5db29abf26e4cd1953edfb2d
Thanaporn Sirirakwongsa <tsirirak@z3t5c1.42bangkok.com>
2022-10-03 04:35:30 +0700

    bonus

--- get_next_line_final/get_next_line_bonus.h
+++ get_next_line_final/get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tsirirak <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/03 02:36:29 by tsirirak          #+#    #+#             */
-/*   Updated: 2022/10/03 04:03:39 by tsirirak         ###   ########.fr       */
+/*   Created: 2022/10/03 04:06:57 by tsirirak          #+#    #+#             */
+/*   Updated: 2022/10/03 04:30:53 by tsirirak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 # include <unistd.h>
 # include <stdlib.h>
 # include <fcntl.h>
@@ -26,8 +26,8 @@ typedef struct s_box
 void	*ft_memset(void *b, int c, size_t len);
 int		ft_strlen(char *s);
 char	*ft_strchr_l(char *s, int i);
-int		len_newline(char *buf, int len_buf, int mode);
-char	*ft_strjoin(char *s1, char *s2, int len_s2);
+int		len_nl(char *buf, int len_buf, int mode);
+char	*ft_join(char *s1, char *s2, int len_s2);
 char	*get_next_line(int fd);
 void	*ft_memset(void *b, int c, size_t len);
 int		ft_strlen(char *s);
